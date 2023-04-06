1852bba74823c211effe2543d537db83edba7d9d
MuhammadAdeel621 <raoadeel960@gmail.com>
2023-04-06 21:14:01 +0500

    Assignment Done

---
+++ app.js
@@ -0,0 +1,128 @@
+//OBJECT 01
+
+// let number1 = +prompt("Enter the First Number : ");
+// let number2 = +prompt("Enter the Second Number : ");
+
+// if (number1 > number2) {
+//   console.log(number1 + " is greatar than " + number2);
+// } else {
+//   console.log(number2 + " is greatar than " + number1);
+// }
+
+//OBJECT 02
+// let numberOne = +prompt("Enter the Number for checking the sign");
+// if (numberOne < 0) {
+//   alert(numberOne + " is a negative Number");
+// } else {
+//   alert(numberOne + " is a Positive Number");
+// }
+
+//OBJECT 03
+// let number1 = +prompt("Enter the First Number : ");
+// let number2 = +prompt("Enter the Second Number : ");
+// let number3 = +prompt("Enter the Third Number : ");
+// let number4 = +prompt("Enter the Fourth Number : ");
+// let number5 = +prompt("Enter the Fifth Number : ");
+
+// if (
+//   number1 > number2 &&
+//   number1 > number3 &&
+//   number1 > number4 &&
+//   number1 > number5
+// ) {
+//   console.log(number1 + " is greatar");
+// } else if (
+//   number2 > number1 &&
+//   number2 > number3 &&
+//   number2 > number4 &&
+//   number2 > number5
+// ) {
+//   console.log(number2 + " is greatar");
+// } else if (
+//   number3 > number1 &&
+//   number3 > number2 &&
+//   number3 > number4 &&
+//   number3 > number5
+// ) {
+//   console.log(number3 + " is greatar");
+// } else if (
+//   number4 > number1 &&
+//   number4 > number2 &&
+//   number4 > number3 &&
+//   number4 > number5
+// ) {
+//   console.log(number2 + " is greatar");
+// } else {
+//   console.log(number5 + " is greatar");
+// }
+
+//OBJECT 04
+// for (let index = 0; index <= 15; index++) {
+//   if (index % 2 === 0) {
+//     console.log(index + " is Even");
+//   } else {
+//     console.log(index + " is Odd");
+//   }
+// }
+
+//OBJECT 05
+// let englishMarks = +prompt("Enter the English Marks : ");
+// let mathMarks = +prompt("Enter the Maths Marks : ");
+// let urduMarks = +prompt("Enter the Urdu Marks : ");
+// let islamiatMarks = +prompt("Enter the Islamiat Marks : ");
+// let chemistryMarks = +prompt("Enter the Chemistry Marks : ");
+// let physicsMarks = +prompt("Enter the Physics Marks : ");
+// let compMarks = +prompt("Enter the Computer Marks : ");
+// let pstMarks = +prompt("Enter the P.S.T Marks : ");
+// let sindhiMarks = +prompt("Enter the Sindhi Marks : ");
+// let bioMarks = +prompt("Enter the Biology Marks : ");
+
+// let sum =
+//   englishMarks +
+//   mathMarks +
+//   urduMarks +
+//   islamiatMarks +
+//   chemistryMarks +
+//   physicsMarks +
+//   compMarks +
+//   pstMarks +
+//   sindhiMarks +
+//   bioMarks;
+
+// let average = sum / 10;
+// if (average > 0 && average < 60) {
+//   console.log("Fails");
+// } else if (average > 60 && average < 70) {
+//   console.log("Garde is : D");
+// } else if (average > 70 && average < 80) {
+//   console.log("Garde is : C");
+// } else if (average > 80 && average < 90) {
+//   console.log("Garde is : B");
+// } else if (average > 90 && average <= 100) {
+//   console.log("Garde is : A");
+// } else {
+//   console.log("Something Went Wrong");
+// }
+
+// Object 06
+// for (let index = 1; index <= 100; index++) {
+//   if (index % 3 === 0 && index % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (index % 5 === 0) {
+//     console.log("Buzz");
+//   } else if (index % 3 === 0) {
+//     console.log("Fizz");
+//   } else {
+//     console.log(index);
+//   }
+// }
+
+// Object 07
+// //outer loop
+// for (i = 1; i <= 5; i++) {
+//   //inner loop
+//   for (j = 1; j <= i; j++) {
+//     document.write("*");
+//   }
+//   document.write("<br/>");
+// }
